d88db5a1c21107d2967c672089551347b05f8fa1
Eleazar Gamez <eleazar.gamezd@gmail.com>
2025-02-16 13:56:44 -0500

    update test command in package.json and add karma configuration file

---
+++ karma.conf.js
@@ -0,0 +1,33 @@
+module.exports = function (config) {
+  config.set({
+    frameworks: ["jasmine", "@angular-devkit/build-angular"],
+    plugins: [
+      require("karma-jasmine"),
+      require("karma-chrome-launcher"),
+      require("karma-jasmine-html-reporter"),
+      require("karma-coverage"),
+      require("@angular-devkit/build-angular/plugins/karma"),
+    ],
+    client: {
+      clearContext: false, // leave Jasmine Spec Runner output visible in browser
+    },
+    coverageReporter: {
+      dir: require("path").join(__dirname, "./coverage"),
+      subdir: ".",
+      reporters: [{ type: "html" }, { type: "text-summary" }],
+    },
+    browsers: ["ChromeHeadless"],
+    customLaunchers: {
+      ChromeHeadless: {
+        base: "Chrome",
+        flags: [
+          "--headless",
+          "--disable-gpu",
+          "--no-sandbox",
+          "--remote-debugging-port=9222",
+        ],
+      },
+    },
+    singleRun: true,
+  });
+};
