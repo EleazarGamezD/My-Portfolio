119374450e4c17ec3ac17cdbce93169aa1a4a034
Eleazar Gamez <eleazar.gamezd@gmail.com>
2025-02-18 23:02:38 -0500

    feat(features): enhance features component layout and improve tech stack display

--- src/app/shared/Components/features/features.component.ts
+++ src/app/shared/Components/features/features.component.ts
@@ -1,8 +1,9 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-features',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './features.component.html',
   styleUrl: './features.component.scss',
 })
