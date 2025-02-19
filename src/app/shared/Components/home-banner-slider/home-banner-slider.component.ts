cb136a99e39a5096266d502d4f411a7394cbe91e
Eleazar Gamez <eleazar.gamezd@gmail.com>
2025-02-18 22:41:22 -0500

    feat(images): remove obsolete image assets and update component references

--- src/app/shared/Components/home-banner-slider/home-banner-slider.component.ts
+++ src/app/shared/Components/home-banner-slider/home-banner-slider.component.ts
@@ -11,22 +11,22 @@ import { HomeSwiperSlideElementComponent } from '../home-swiper-slide-element/ho
 export class HomeBannerSliderComponent {
   sliderContentArray: HomeBannerSlider[] = [
     {
-      title: 'Software Developer',
-      description:
-        'I am a software developer with a passion for creating innovative solutions that make a difference.',
-      image: '/assets/images/home-banner-slider/bg-1.webp',
-    },
-    {
       title: 'Web Developer',
       description:
-        'I am a web developer with a passion for creating beautiful and functional websites.',
-      image: '/assets/images/home-banner-slider/keyboard.webp',
+        'I am a software developer with a passion for creating innovative solutions that make a difference.',
+      image: '/assets/images/backgrounds/bg-1.webp',
     },
     {
-      title: 'Mobile Developer',
+      title: 'Backend Developer',
+      description:
+        'I am a web developer with a passion for creating beautiful and functional websites.',
+      image: '/assets/images/backgrounds/keyboard.webp',
+    },
+    {
+      title: 'Frontend Developer',
       description:
         'I am a mobile developer with a passion for creating beautiful and functional mobile applications.',
-      image: '/assets/images/home-banner-slider/desktop-v3.webp',
+      image: '/assets/images/backgrounds/wallpaperflare.com_wallpaper.jpg',
     },
   ];
 }
