05eb03626113a5f95ead7d4603f1619597c50b8a
ELEAZAR GAMEZ <eleazar.gamezd@gmail.com>
2025-02-21 19:11:48 -0500

    Actualizar environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   reCaptchaSiteKey: '6LeLw94qAAAAAE1OPB7Myosf-WoKY3FZdWeO0YYH',
-  reCaptchaSecretKey: '6LeLw94qAAAAAPzoSAWhNEdZ5T6JJ5uOvCZVLt_T',
+  reCaptchaSecretKey: '',
   apiUrl: 'https://api.eleazargamez.dev',
 };
