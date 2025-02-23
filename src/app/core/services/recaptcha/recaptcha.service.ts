bae32d6a13768961f64cd2300dcc84398dd94747
Eleazar Gamez <eleazar.gamezd@gmail.com>
2025-02-23 03:19:50 -0500

    feat(contact-me): migrate to ng-recaptcha3 and update contact form for reCAPTCHA token handling

--- src/app/core/services/recaptcha/recaptcha.service.ts
+++ src/app/core/services/recaptcha/recaptcha.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { RequestMethod } from '@core/enum/globalHttpRequest/globalHttpRequest.enum';
+import { IRecaptchaVerifyResponse } from '@core/interfaces/recaptcha/recaptchat.interface';
 import { API_RECAPTCHA_ROUTES } from '@core/routes/recaptcha/recaptcha.routes';
 import { GlobalHttpService } from '@services/globalHttp/global-http.service';
 
@@ -7,8 +8,8 @@ import { GlobalHttpService } from '@services/globalHttp/global-http.service';
   providedIn: 'root',
 })
 export class RecaptchaService extends GlobalHttpService {
-  async verifyToken(token: string): Promise<boolean> {
-    return await this.makeHttpRequest<boolean>(
+  async verifyToken(token: string): Promise<IRecaptchaVerifyResponse> {
+    return await this.makeHttpRequest<IRecaptchaVerifyResponse>(
       API_RECAPTCHA_ROUTES.checkRecaptcha,
       { token },
       RequestMethod.POST,
