8478ca07983a02370be85f3cef4dd5f5361579c7
EleazarrGamezD <eleazar.gamezd@gmail.com>
2026-05-03 23:01:16 -0500

    feat: update environment configuration and add environment files to .gitignore

---
+++ src/environments/environment.local.example.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+    appName: 'Portfolio-Local-Example',
+  reCaptchaSiteKey: 'captcha_site_key',
+  backendApiKey: 'replace-with-a-long-random-secret',
+  apiUrl: 'url_to_api',
+};
--- src/environments/environment.local.ts
+++ src/environments/environment.local.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   appName: 'EG-Portfolio-Local',
-  reCaptchaSiteKey: '6Led598qAAAAANaemWcsYVh-reJi-10YHPxiRZRQ',
+  reCaptchaSiteKey: '6Lf0QOAqAAAAALLBwbkS12Z4BppSOzjaw3Z1lfBX',
   backendApiKey: 'replace-with-a-long-random-secret',
   apiUrl: 'http://localhost:3000/api',
 };
